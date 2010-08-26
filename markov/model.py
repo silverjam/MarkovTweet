3cf60a85797ae6b3ee3b72b243400706283d87ec
Jason Mobarak <jason@mobarak.name>
2010-08-25 21:19:55 -0600

    Clean up cruft copied from another project.

--- markov/model.py
+++ markov/model.py
@@ -8,16 +8,3 @@ class MarkovUserV1(db.Model):
     name = db.StringProperty()
     tweets = db.BlobProperty()
     generated = db.StringProperty()
-
-"""
-class V1_ValidEmail(db.Model):
-    email = db.StringProperty()
-
-class V1_TokenCounter(db.Model):
-    counter = db.IntegerProperty()
-
-class V1_Reminder(db.Model):
-    token = db.StringProperty()
-    message = db.TextProperty() 
-    time  = db.DateTimeProperty()
-"""
